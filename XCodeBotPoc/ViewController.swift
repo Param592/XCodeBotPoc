f0cb38604ece77ed16f49614c39b163da9e54d39
Arunava Sanyal <paramjeet.2.kaur@bt.com>
2019-09-17 17:57:23 +0530

    Initial commit to repo

---
+++ XCodeBotPoc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XCodeBotPoc
+//
+//  Created by Arunava Sanyal on 17/09/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
